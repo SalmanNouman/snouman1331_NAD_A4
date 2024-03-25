df359dd9e7bfbff238b3f02347e241fe1ab28983
SalmanNouman <114771256+SalmanNouman@users.noreply.github.com>
2024-03-25 16:09:30 -0400

    Manipulating DOM using views through javascript
    Added functionality to load postsdata using an Ajax call

--- src/posts/static/posts/main.js
+++ src/posts/static/posts/main.js
@@ -1,3 +1,18 @@
-const helloWorldBox = document.getElementById('hello-world')
+const postsBox = document.getElementById('posts-box')
 
-helloWorldBox.innerHTML = 'Hello <b>world</b>'
+
+$.ajax({
+    type: 'GET',
+    url: '/data/',
+    success: function(response){
+        const data = response.data;
+        data.forEach(el => {
+            postsBox.innerHTML += `
+                ${el.title} - <b>${el.body}</b><br>
+            `
+        });
+    },
+    error: function(error){
+        console.log('error', error)
+    }
+})
