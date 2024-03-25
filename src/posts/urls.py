ee297f4839b843330f764ae544d3ce61464e86c5
SalmanNouman <114771256+SalmanNouman@users.noreply.github.com>
2024-03-25 18:38:00 -0400

    Added like button functionality with Ajax POST requests

--- src/posts/urls.py
+++ src/posts/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from .views import (
     post_list_and_create,
-    hello_world_view,
     load_posts_data_view,
+    like_unlike_post,
 )
 
 app_name = 'posts'
@@ -10,5 +10,5 @@ app_name = 'posts'
 urlpatterns = [
     path('', post_list_and_create, name='main-board'),
     path('data/<int:num_posts>/', load_posts_data_view, name='posts-data'),
-    path('hello-world/', hello_world_view, name='hello-world'),
+    path('like-unlike/', like_unlike_post, name='like-unlike'),
 ]
