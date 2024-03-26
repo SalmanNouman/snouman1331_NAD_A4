3f7b7ee65e0a01cfaa23a4dc60e95592020a00c7
SalmanNouman <114771256+SalmanNouman@users.noreply.github.com>
2024-03-25 23:20:02 -0400

    Added modals for update and delete buttons in Details page

--- src/posts/static/posts/detail.js
+++ src/posts/static/posts/detail.js
@@ -1,9 +1,12 @@
 const backBtn = document.getElementById('back-btn')
 const updateBtn = document.getElementById('update-btn')
 const deleteBtn = document.getElementById('delete-btn')
+const postBox = document.getElementById('post-box')
 const url = window.location.href + "/data/"
 const spinnerBox = document.getElementById('detail-spinner')
 
+const titleInput = document.getElementById('id_title')
+const bodyInput = document.getElementById('id_body')
 
 backBtn.addEventListener('click', () => {
     history.back()
@@ -20,6 +23,21 @@ $.ajax({
             updateBtn.classList.remove('not-visible')
             deleteBtn.classList.remove('not-visible')
         }
+
+        const titleEl = document.createElement('h3')
+        titleEl.setAttribute('class', 'mt-3')
+        const bodyEl = document.createElement('p')
+        bodyEl.setAttribute('class', 'mt-1')
+
+        titleEl.textContent = data.title
+        bodyEl.textContent = data.body
+
+        postBox.appendChild(titleEl)
+        postBox.appendChild(bodyEl)
+
+        titleInput.value = data.title
+        bodyInput.value = data.body
+
         spinnerBox.classList.add('not-visible')
     },
     error: function(error){
