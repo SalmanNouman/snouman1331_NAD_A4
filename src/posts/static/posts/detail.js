69a97487bf8a3d33613b12de1c846bd8ab9ab444
SalmanNouman <114771256+SalmanNouman@users.noreply.github.com>
2024-03-25 23:00:34 -0400

    Added favicon and more functionality to the details HTML page

--- src/posts/static/posts/detail.js
+++ src/posts/static/posts/detail.js
@@ -1,4 +1,6 @@
 const backBtn = document.getElementById('back-btn')
+const updateBtn = document.getElementById('update-btn')
+const deleteBtn = document.getElementById('delete-btn')
 const url = window.location.href + "/data/"
 const spinnerBox = document.getElementById('detail-spinner')
 
@@ -11,8 +13,13 @@ $.ajax({
     type: 'GET',
     url: url,
     success: function(response){
-        console.log(url)
-        console.log(response)
+        const data = response.data
+        if (data.logged_in !== data.author){
+            console.log('different')
+        } else {
+            updateBtn.classList.remove('not-visible')
+            deleteBtn.classList.remove('not-visible')
+        }
         spinnerBox.classList.add('not-visible')
     },
     error: function(error){
